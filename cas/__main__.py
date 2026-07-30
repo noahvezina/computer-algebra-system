892cdc1c6f627ab40069db3aa25389b4046ee738
Noah Vezina <noahvezina26@gmail.com>
2026-07-29 21:12:44 -0400

    Rename main, add dunder init

---
+++ cas/__main__.py
@@ -0,0 +1,36 @@
+import cmd
+
+from cas.lexer import Lexer
+from cas.parser import Parser
+from cas.exceptions import LexerException, ParserException
+
+
+class ComputerAlegbraSystem(cmd.Cmd):
+
+    intro = "Computer Algebra System in Python."
+    prompt = ">>> "
+
+    def default(self, line: str) -> None:
+        """Run line."""
+        return run(line)
+
+    def do_quit(self, line: str) -> None:
+        """Exit the REPL."""
+        return True
+
+
+def run(user_input):
+
+    lexer = Lexer(user_input)
+
+    try:
+        tokens = lexer.scanTokens()
+    except LexerException as error:
+        print(f"Error [col {error.column}]: {error.message}")
+        return None
+
+    print(tokens)
+
+
+if __name__ == "__main__":
+    ComputerAlegbraSystem().cmdloop()
