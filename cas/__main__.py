38ab404e759a4bebc595c25ffc0b2764df7ce409
Noah Vezina <noahvezina26@gmail.com>
2026-08-08 17:28:18 -0400

    Put everything inside CAS class

--- cas/__main__.py
+++ cas/__main__.py
@@ -10,37 +10,50 @@ class ComputerAlegbraSystem(cmd.Cmd):
     intro = "Computer Algebra System in Python."
     prompt = ">>> "
 
+    # Static function names --- maybe this should be imported from functions.py when written
+    func_names = ["sin", "cos", "tan", "sqrt"]
+
+    def __init__(self, completekey = "tab", stdin = None, stdout = None):
+        super().__init__(completekey, stdin, stdout)
+
+        # Storing session context
+        self.context = {
+            "func_names": self.func_names,
+            "saved_exprs": []
+            }
+
     def default(self, line: str) -> None:
         """Run line."""
-        return run(line)
+        self.run(line)
+        return None
 
     def do_quit(self, line: str) -> None:
         """Exit the REPL."""
         return True
 
+    def run(self, user_input: str) -> None:
 
-def run(user_input):
+        lexer = Lexer()
+        try:
+            tokens = lexer.tokenize(user_input)
+        except LexerException as error:
+            self.printError(error.message, error.column)
 
-    lexer = Lexer()
-    try:
-        tokens = lexer.tokenize(user_input)
-    except LexerException as error:
-        printError(error.column, error.message)
-        return None
+        parser = Parser()
+        try:
+            ast = parser.parse(tokens, self.context)
+        except ParserException as error:
+            self.printError(error.message, error.column)
 
-    parser = Parser()
-    try:
-        ast = parser.parse(tokens)
-    except ParserException as error:
-        printError(error.column, error.message)
-        return None
+        print("Tokens")
 
-    print("Tokens")
-
-
-def printError(column: int, message: str) -> None:
-    print(f"Error [col {column}]: {message}")
+    def printError(message: str, column: int = -1) -> None:
+        if column:
+            print(f"Error [col {column}]: {message}")
+        else:
+            print(f"Error: {message}")
 
 
 if __name__ == "__main__":
-    ComputerAlegbraSystem().cmdloop()
+    cas = ComputerAlegbraSystem()
+    cas.cmdloop()
