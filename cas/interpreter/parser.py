eeee0487cfe11106bb577a4e6fca1f76fdd4cd63
Noah Vezina <noahvezina26@gmail.com>
2026-08-09 14:21:37 -0400

    Write two-pass rewrite method for identifiers and implicit multiplication

--- cas/interpreter/parser.py
+++ cas/interpreter/parser.py
@@ -17,16 +17,77 @@ call    :=  ID "(" [ expr  { "," expr } ] ")"
 
 
 class Parser:
+    """A parser that transforms tokens into a meaningful AST."""
 
     def __init__(self) -> None:
         self._current = 0
 
-    def parse(self, tokens) -> Stmt:
-        """Parse tokens into meaningful syntax trees."""
+    def parse(self, tokens: list[Token], context: dict[list]) -> Stmt:
+        """Parse tokens into meaningful syntax trees using recursive descent."""
         self._tokens = tokens
-        if self._match(TokenType.LET):
-            return self._decl()
-        return self._expr()
+        self._context = context
+
+        self._rewrite()
+
+        # if self._match(TokenType.LET):
+        #     return self._decl()
+        # return self._expr()
+
+    def _rewrite(self) -> None:
+        """A two-pass rewrite. Chop up identifiers (variables, function calls, and saved expressions) then add implicit multiplication tokens where suitable."""
+
+        # We iterate in reverse so that the index stays accurate when adding elements to the list
+        for i in reversed(range(len(self._tokens))):
+            if self._tokens[i].type == TokenType.IDENTIFIER:
+                text = self._tokens[i].text
+                new_identifiers = self._chopIdentifiers(text)
+                self._tokens[i:i + 1] = new_identifiers
+            # We do not want to break up declaraction names
+            if self._tokens[i].type == TokenType.COLON_EQUALS:
+                break
+
+        # Accepted implicit multiplication token pairs (i.e. if a NUMBER token is followed by an IDENTIFIER token, it implies multiplication)
+        implicits = {
+            TokenType.NUMBER: [TokenType.IDENTIFIER, TokenType.L_PAREN],
+            TokenType.IDENTIFIER: [TokenType.IDENTIFIER, TokenType.NUMBER, TokenType.L_PAREN],
+            TokenType.R_PAREN: [TokenType.IDENTIFIER, TokenType.NUMBER, TokenType.L_PAREN]
+            }
+
+        # Same reverse idea here, but note that the implicits dictionary is relative to the forward direction
+        for i in reversed(range(1, len(self._tokens))):
+            left = self._tokens[i - 1]
+            right = self._tokens[i]
+            if left.text in self._context["func_names"]:
+                continue
+            elif left.type in implicits and right.type in implicits[left.type]:
+                implicit_token = Token(TokenType.IMPLICIT, "")
+                self._tokens.insert(i, implicit_token)
+
+    def _chopIdentifiers(self, text: str) -> list[Token]:
+        """Take apart an indentifier into variables, function calls, and saved expressions."""
+        substring_start = 0
+        new_identifiers = []
+
+        while substring_start < len(text):
+            substring_end = substring_start + 1
+            substring_found = False
+            # Looking for multi-character substrings
+            while substring_end <= len(text):
+                if text[substring_start:substring_end] in self._context["all_reserved"]:
+                    new_token = Token(TokenType.IDENTIFIER, text[substring_start:substring_end])
+                    new_identifiers.append(new_token)
+                    substring_start = substring_end
+                    substring_found = True
+                else:
+                    substring_end += 1
+            # None found, so we'll add a single-char substring
+            if not substring_found:
+                substring_end = substring_start + 1
+                new_token = Token(TokenType.IDENTIFIER, text[substring_start:substring_end])
+                new_identifiers.append(new_token)
+                substring_start = substring_end
+
+        return new_identifiers
 
     def _decl(self) -> Decl:
         """Get declaration statement."""
@@ -55,13 +116,11 @@ class Parser:
         return expr
 
     def _mult(self) -> Expr:
-        """Get multiplication expression. Handles implicit multiplication.
-        3x^2 + xsin(x) - sqrt(x)x + abc/23x - 9quadratic + 1/9
-        """
+        """Get multiplication expression. Handles implicit multiplication."""
         expr = self._pow()
         factors = []
 
-        while self._match(TokenType.STAR, TokenType.SLASH):
+        while self._match(TokenType.STAR, TokenType.SLASH, TokenType.IMPLICIT):
             if self._previous().type == TokenType.SLASH:
                 factors.append(self._pow())  # Gotta figure out how to make this negative
             else:
@@ -99,7 +158,7 @@ class Parser:
         return self._tokens[self._current]
 
     def _match(self, types: list[TokenType]) -> bool:
-        """Check if the next token is of any of the given types, advance token too."""
+        """Check if the next token is of any of the given types, if so it advances to the next token."""
         for type in types:
             if self._check(type):
                 self._advance()
@@ -111,7 +170,7 @@ class Parser:
         if self._check(type):
             return self._advance()
         column = self._current + 1
-        raise ParserException(column, message)
+        raise ParserException(message)
 
     def _check(self, type: TokenType) -> bool:
         """Check if the next token is of the given type. Does not advance token."""
@@ -129,4 +188,4 @@ class Parser:
 
 
 if __name__ == "__main__":
-    pass
+    parser = Parser()
