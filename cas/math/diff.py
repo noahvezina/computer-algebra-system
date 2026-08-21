f3e4f36c8e8b638d36480578afa0305b855d8206
Noah Vezina <noahvezina26@gmail.com>
2026-08-20 20:41:31 -0400

    Clean up code a bit for differentiation

--- cas/math/diff.py
+++ cas/math/diff.py
@@ -1,14 +1,14 @@
 from cas.interpreter.nodes import Expr, Add, Mult, Pow, Var, Num, Int
 
 
-def diff(expr: Expr, var: Var):
-    """Differentiate an expression with respect to var."""
+def diff(expr: Expr):
+    """Differentiate an expression with respect to one variable."""
     match expr:
         case Add(terms):
             # Sum rule
             result = []
             for term in terms:
-                result.append(diff(term, var))
+                result.append(diff(term))
             return Add(*result)
         case Mult(factors):
             # Product rule
@@ -17,7 +17,7 @@ def diff(expr: Expr, var: Var):
                 result_factors = []
                 for j in range(len(factors)):
                     if i == j:
-                        result_factors.append(diff(factors[j], var))
+                        result_factors.append(diff(factors[j]))
                     else:
                         result_factors.append(factors[j])
                 result_terms.append(Mult(*result_factors))
@@ -25,12 +25,10 @@ def diff(expr: Expr, var: Var):
         case Pow(base, exponent):
             # Power rule
             if isinstance(exponent, Num):
-                return Mult(diff(base, var), Pow(base, exponent - Int(1)))
+                return Mult(diff(base), Pow(base, exponent - Int(1)))
         case Num():
-            # Number
             return Int("0")
         case Var() as var:
-            # Variable
             return Int("1")
         case _:
             return "Not implemented yet"
